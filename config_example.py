92e1d1852d8c670624ee4c66091ce7734c9de243
Ruby Simonpietri <jasxy4321@gmail.com>
2023-07-18 16:10:38 -0400

    added comments to help people understand what each thing does removed ruby_id and replaces with mod_id, (done in main too)

--- config_example.py
+++ config_example.py
@@ -1,7 +1,11 @@
 # config_example.py
 
+# token should be a string
 TOKEN = "YOUR_BOT_TOKEN"
-GUILD = GUILD_ID (the discord server where you want the bot)
-RANKING_CHANNEL = RANKING_CHANNEL_ID (where you want rankings to be displayed)
-COMMAND_CHANNEL = COMMAND_CHANNEL_ID (where commands will be sent)
-RUBY_ID = USER_ID_OF_ADMIN (ill change it later)
+# guild id should be an int and should be the id of the server you want the bot to send messages in
+GUILD = 123123123123
+# ranking channel should be an int and should be the id of the channel you want the bot to display album rankings in
+RANKING_CHANNEL = 456456456456
+# if you want to use it on your server, it should be the owner's id
+# (this allows the mods to run mod commands)
+MOD_ID = 123456789
