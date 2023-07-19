6655d918c6b97f9fe7b313c908f488ccaaea9aec
Ruby Simonpietri <jasxy4321@gmail.com>
2023-07-19 16:29:11 -0400

    literally im the pickiest formatter so i added a blank line

--- spotify_integration.py
+++ spotify_integration.py
@@ -4,6 +4,7 @@ import spotify.sync as spotify
 spotifyClient = spotify.Client(config.SPOTIFY_CLIENT_ID, config.SPOTIFY_CLIENT_SECRET)
 spotifyUser = spotify.User.from_refresh_token(spotifyClient, config.SPOTIFY_REFRESH_TOKEN)
 
+
 def get_album(artist, album_name):
     results = spotifyClient.search(f"{album_name} {artist}", types=["album"], limit=1)
     album = results[2][0]
