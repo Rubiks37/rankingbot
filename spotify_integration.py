0c365246c8de9aa4131de671105fd0db56370b48
Ruby Simonpietri <jasxy4321@gmail.com>
2023-07-22 15:57:56 -0400

    added a close function, made things a bit more friendly

--- spotify_integration.py
+++ spotify_integration.py
@@ -5,8 +5,13 @@ spotifyClient = spotify.Client(config.SPOTIFY_CLIENT_ID, config.SPOTIFY_CLIENT_S
 spotifyUser = spotify.User.from_refresh_token(spotifyClient, config.SPOTIFY_REFRESH_TOKEN)
 
 
-def get_album(artist, album_name):
-    results = spotifyClient.search(f"{album_name} {artist}", types=["album"], limit=1)
+def get_album(artist_name, album_name):
+    results = spotifyClient.search(f"{album_name} {artist_name}", types=["album"], limit=1)
     album = results[2][0]
 
-    return (album.id, album.name, album.artists[0].name, album.images[0].url)
+    return album.id, album.name, album.artists[0].name, album.release_date, album.images[0].url
+
+
+def spotify_close_conn():
+    spotifyUser.close()
+    spotifyClient.close()
