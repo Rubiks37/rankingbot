4895f9e3a8a6f9137ce1e154c42fbbd18bfed75a
Ruby Simonpietri <jasxy4321@gmail.com>
2023-07-22 16:33:02 -0400

    properly escaped all sql inputs, reformatted

--- main.py
+++ main.py
@@ -88,7 +88,8 @@ def make_table(user_id):
 
 def make_album_master():
     cursor = conn.cursor()
-    cursor.execute(f'''CREATE TABLE IF NOT EXISTS album_master (artist TEXT, album TEXT, id TEXT, year INTEGER, image TEXT)''')
+    cursor.execute(f'''CREATE TABLE IF NOT EXISTS album_master'''
+                   f'''(artist TEXT, album TEXT, id TEXT, year INTEGER, image TEXT)''')
     cursor.close()
     return
 
@@ -106,7 +107,8 @@ async def add_to_album_master(artist: str, album: str):
     if get_album_master_row(album=row[1], artist=row[2], abort_early=True) is not None:
         return None
     year = row[3].split('-')[0]
-    cursor.execute(f'''INSERT INTO album_master (artist, album, id, year, image) VALUES ('{row[2]}', '{row[1]}', '{row[0]}', '{year}', '{row[4]}')''')
+    cursor.execute(f'''INSERT INTO album_master (artist, album, id, year, image) VALUES (?, ?, ?, ?, ?)''',
+                   (row[2], row[1], row[0], year, row[4]))
     if cursor.rowcount < 1:
         raise LookupError("error: no rows were added, huh why what how???")
     cursor.close()
@@ -254,7 +256,8 @@ async def add_row(user_id, artist: str, album: str, rating: float):
     await add_to_album_master(artist=artist, album=album)
     row = get_album_master_row(artist=artist, album=album)
     # strip the message of extraneous characters, add to the table
-    cursor.execute(f'''INSERT INTO user_data_{user_id} (artists, title, rating) VALUES('{row[0]}', '{row[1]}', '{rating}')''')
+    cursor.execute(f'''INSERT INTO user_data_{user_id} (artists, title, rating)'''
+                   f'''VALUES(?, ?, ?)''', (row[0], row[1], rating))
     if cursor.rowcount < 1:
         raise LookupError("error: no rows were added, which is confusing idk why that happened")
     conn.commit()
@@ -286,7 +289,8 @@ def edit_row(user_id, row, rating: float):
     # prevent an index out of bound exception
     # update rating value in corresponding row
     cursor = conn.cursor()
-    cursor.execute(f'''UPDATE user_data_{user_id} SET rating = {rating} WHERE artists = ? AND title = ?''', (row[0], row[1]))
+    cursor.execute(f'''UPDATE user_data_{user_id} SET rating = {rating}'''
+                   f'''WHERE artists = ? AND title = ?''', (row[0], row[1]))
     if cursor.rowcount < 1:
         raise LookupError("error: no rows were edited, which is confusing idk why that happened")
     conn.commit()
