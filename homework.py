79e2117f4a443850017d11ad9b68d11c3d9b02e1
Ruby Simonpietri <jasxy4321@gmail.com>
2023-08-04 16:08:34 -0400

    added get_homework_formatted, moved it out of get_homework idk why but i also changed add_homework, maybe ill change it back idk

--- homework.py
+++ homework.py
@@ -1,28 +1,35 @@
 import spotify_integration as spotify
 
+
 def create_homework_table(conn):
-    conn.execute('''CREATE TABLE IF NOT EXISTS homework(album_id TEXT, user_id INTEGER, complete BIT, PRIMARY KEY("user_id","album_id"))''')
+    conn.execute('''CREATE TABLE IF NOT EXISTS homework(
+    album_id TEXT, user_id INTEGER, complete BIT, PRIMARY KEY("user_id","album_id"))''')
     conn.commit()
 
-def add_homework(conn, user_id, album_id):
-    try:
-        create_homework_table(conn)
-        cursor = conn.cursor()
-        cursor.execute('''INSERT INTO homework (album_id, user_id, complete) VALUES (?, ?, 0)''', (album_id, user_id))
-        cursor.close()
-        conn.commit()
-    except:
-        pass
 
-def get_homework(conn, user, complete=0):
+def add_homework(conn, user_id, album_id):
+    create_homework_table(conn)
+    cursor = conn.cursor()
+    cursor.execute('''INSERT INTO homework (album_id, user_id, complete) VALUES (?, ?, 0) RETURNING *''', (album_id, user_id))
+    data = cursor.fetchall()
+    cursor.close()
+    conn.commit()
+    return f"successfully added {len(data)} row to homework"
+
+
+def get_homework(conn, user_id, complete=0):
     create_homework_table(conn)
     cursor = conn.cursor()
     cursor.execute(f'''SELECT * FROM homework
                    INNER JOIN album_master ON homework.album_id = album_master.id
-                   WHERE user_id = {user.id} AND complete = {complete}''')
+                   WHERE user_id = ? AND complete = ?''', (user_id, complete))
     data = cursor.fetchall()
     cursor.close()
+    return data
 
+
+def get_homework_formatted(conn, user, complete=0):
+    data = get_homework(conn, user.id, complete)
     output = f'## Homework of {user.mention}\n'
     for i, row in enumerate(data):
         output += f'{i + 1}. {row[3]} - {row[4]} ({row[6]})\n'
@@ -30,9 +37,28 @@ def get_homework(conn, user, complete=0):
         output += f"{user.display_name} doesn't have any homework at the moment\n"
     return output + f"\nPlaylist URL: {spotify.get_playlist(user).url}"
 
+
+# this is basically just making sure that an album exists for update_album_master
+def get_homework_row(conn, album_id):
+    create_homework_table(conn)
+    cursor = conn.cursor()
+    cursor.execute(f'''SELECT * FROM homework
+                   INNER JOIN album_master ON homework.album_id = album_master.id
+                   WHERE album_id = ?''', (album_id,))
+    data = cursor.fetchall()
+    if len(data) == 0:
+        return None
+    cursor.close()
+    return data[0]
+
+
 def remove_homework(conn, user_id, album_id):
     create_homework_table(conn)
     cursor = conn.cursor()
-    cursor.execute('''DELETE FROM homework WHERE album_id = ? AND user_id = ?''', (album_id, user_id))
+    cursor.execute('''DELETE FROM homework WHERE album_id = ? AND user_id = ? RETURNING *''', (album_id, user_id))
+    data = cursor.fetchall()
     cursor.close()
     conn.commit()
+    if data is None:
+        raise ValueError("error: row not found in your homework")
+    return f"successfully deleted {len(data)} rows from homework"
