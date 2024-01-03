09535cbaaa00e8a307b21e3efb0fc1282e9aa426
Rubiks37 <jasxy4321@gmail.com>
2024-01-03 18:51:33 -0500

    adjusted most autocompletes for new formatting of tables

--- autocomplete.py
+++ autocomplete.py
@@ -43,7 +43,7 @@ def autocomplete_search_list(current_str: str, strs_to_search: tuple):
 
 def filter_year(all_ratings, year):
     # filters albums by year (or does not filter if year filtering is turned off)
-    return {key: value for key, value in all_ratings.items() if year == -1 or year == key[2]}
+    return [row for row in all_ratings if year == -1 or year == row['year']]
 
 
 def get_max_albums_possible(get_all_ratings,  year=datetime.now().year, minratings: int = None):
@@ -51,18 +51,19 @@ def get_max_albums_possible(get_all_ratings,  year=datetime.now().year, minratin
     if minratings is None:
         return len(ratings_year)
     # filters albums - if the number of ratings is greater than the min_ratings
-    final_dict = {key: value for key, value in ratings_year.items() if len(value) >= minratings}
+    final_dict = {row for row in ratings_year if len(row['ratings']) >= minratings}
     return len(final_dict)
 
 
 def get_min_ratings_possible(get_all_ratings, year=datetime.now().year, numalbums: int = None):
     ratings_year = filter_year(get_all_ratings(), year)
+
     # sort the dictionary by number of ratings,
     # then go down however many rows and return the number of ratings on that album
-    final_dict = dict(sorted(ratings_year.items(), key=lambda row: len(row[1]), reverse=True))
+    final_dict = list(sorted(ratings_year, key=lambda row: len(row['ratings']), reverse=True))
     if numalbums is None:
-        return len(list(final_dict.values())[len(final_dict) - 1])
-    return len(list(final_dict.values())[numalbums - 1])
+        return len(final_dict[-1])
+    return len(final_dict[numalbums - 1])
 
 
 # this takes in a list of (artist, album, value) and returns that list with all entries as less than 100 characters
@@ -73,7 +74,7 @@ def autocomplete_slice_list_names(choices):
 # gets a list of artists in album_master and returns a list of choices for use in autocomplete
 def autocomplete_artist(get_album_master):
     async def inner(interaction: Interaction, current: str) -> list[Choice[str]]:
-        final_list = {", ".join(row[0]) for row in get_album_master()}
+        final_list = {", ".join(row['artist']) for row in get_album_master()}
         return [Choice(name=artist, value=artist)
                 for artist in final_list if strip_names(current)[0] in strip_names(artist)[0]][:25]
     return inner
@@ -83,7 +84,7 @@ def autocomplete_artist(get_album_master):
 # get_album_master is a refernece to the function in main
 def autocomplete_album(get_album_master):
     async def inner(interaction: Interaction, current: str) -> list[Choice[str]]:
-        albums = tuple((row[1],) for row in get_album_master())
+        albums = tuple((row['album_name']) for row in get_album_master())
         final_list = autocomplete_slice_list_names(albums)
         return [Choice(name=album[0], value=album[0])
                 for album in final_list if strip_names(current)[0] in strip_names(album[0])[0]][:25]
@@ -94,7 +95,7 @@ def autocomplete_album(get_album_master):
 def autocomplete_spotify(search_album):
     async def inner(interaction: Interaction, current: str) -> list[Choice[str]]:
         results = await search_album(current)
-        choices = [(", ".join(tuple(artist.name for artist in entry.artists)) + f" ({entry.type}) - {entry.name}", f"{entry.id}") for entry in results]
+        choices = [(", ".join([artist.name for artist in entry.artists]) + f" ({entry.type}) - {entry.name}", f"{entry.id}") for entry in results]
         final_choices = autocomplete_slice_list_names(choices)
         return [Choice(name=f"{choice[0]}", value=f"{choice[1]}") for choice in final_choices][:25]
     return inner
@@ -103,9 +104,9 @@ def autocomplete_spotify(search_album):
 # gets a formatted list of choices of artist - album using album_master
 def autocomplete_artist_album(get_album_master):
     async def inner(interaction: Interaction, current: str) -> list[Choice[str]]:
-        user_albums = tuple([(", ".join(row[0]) + f" - {row[1]}", f"{row[2]}") for row in get_album_master()])
-        half_final_list = autocomplete_search_list(current, user_albums)
-        final_list = autocomplete_slice_list_names(half_final_list)
+        user_albums = tuple([(", ".join(row['artist']) + f" - {row['album_name']}", f"{row['album_id']}") for row in get_album_master()])
+        filtered_list = autocomplete_search_list(current, user_albums)
+        final_list = autocomplete_slice_list_names(filtered_list)
         return [Choice(name=entry[0], value=entry[1]) for entry in final_list][:25]
     return inner
 
@@ -113,8 +114,10 @@ def autocomplete_artist_album(get_album_master):
 # gets formatted choices for artist/album when editing/deleting rows from the list
 def autocomplete_artist_album_user_specific(get_rows_from_user):
     async def inner(interaction: Interaction, current: str) -> list[Choice[str]]:
-        albums = [(", ".join(row[0]) + f" - {row[1]}", f"{row[3]}") for row in get_rows_from_user(interaction.user.id)]
+        albums = [(", ".join(row['artist']) + f" - {row['album_name']}", f"{row['album_id']}")
+                  for row in get_rows_from_user(interaction.user.id)]
         final_list = autocomplete_slice_list_names(albums)
+
         return [Choice(name=entry[0], value=entry[1])
                 for entry in final_list if current.lower() in entry[0].lower()][:25]
     return inner
@@ -123,8 +126,8 @@ def autocomplete_artist_album_user_specific(get_rows_from_user):
 # gets formatted choices for artist/album when editing/deleting rows from homework
 def autocomplete_artist_album_homework_specific(get_homework, conn):
     async def inner(interaction: Interaction, current: str) -> list[Choice[str]]:
-        homework_list = [(", ".join(row[3]) + f" - {row[4]}", f"{row[5]}")
-                         for row in get_homework(conn=conn, user_id=interaction.user.id)]
+        homework_list = [(", ".join(row['artist']) + f" - {row['album_name']}", f"{row['album_id']}")
+                         for row in get_homework(user_id=interaction.user.id)]
         final_list = autocomplete_slice_list_names(homework_list)
         return [Choice(name=entry[0], value=entry[1])
                 for entry in final_list if current.lower() in entry[0].lower()][:25]
