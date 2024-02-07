b26898cb479e2d31c76e852705018ae083ebd36b
Rubiks37 <jasxy4321@gmail.com>
2024-02-07 14:06:15 -0500

    updated some changelog function calls to represent how updated functions return things

--- changelog.py
+++ changelog.py
@@ -24,50 +24,51 @@ class Changelog:
         def wrapper(self, *args, **kwargs):
             if self.changelog_active:
                 return inner(self, *args, **kwargs)
+            return None
         return wrapper
 
     @check_decorator
     async def event_add_ranking(self, user, album_id, rating, users=None):
         if user not in users:
             await self.channel.send(f"{user.mention} has just used ranking bot for the first time lfggggggg")
-        row = get_album(album_id=album_id)
-        artist = ", ".join(row[0])
-        await self.channel.send(f"RANKINGS - {user.mention}:\n`rated {artist} - {row[1]} as a {rating}`")
+        album = get_album(album_id=album_id)
+        artist = ", ".join([artist.name for artist in album.artists])
+        await self.channel.send(f"RANKINGS - {user.mention}:\n`rated {artist} - {album.name} as a {rating}`")
 
     # in an edited album, the changes parameter is the album_id, the old rating, and the new rating
     @check_decorator
     async def event_edit_ranking(self, user, album_id, old_rating, new_rating):
-        row = get_album(album_id=album_id)
-        artist = ", ".join(row[0])
-        await self.channel.send(f"RANKINGS - {user.mention}:\n`changed {artist} - {row[1]} from a {old_rating}/10.0 to a {new_rating}/10.0`")
+        album = get_album(album_id=album_id)
+        artist = ", ".join([artist.name for artist in album.artists])
+        await self.channel.send(f"RANKINGS - {user.mention}:\n`changed {artist} - {album.name} from a {old_rating}/10.0 to a {new_rating}/10.0`")
 
     # in a removed album, the changes parameter is the album_id
     # we need to use spotify api since it may not be in album_master after the removal
     @check_decorator
     async def event_remove_ranking(self, user, album_id):
-        row = get_album(album_id=album_id)
-        artist = ", ".join(row[0])
-        await self.channel.send(f"RANKINGS - {user.mention}:\n`removed {artist} - {row[1]} from their rankings`")
+        album = get_album(album_id=album_id)
+        artist = ", ".join([artist.name for artist in album.artists])
+        await self.channel.send(f"RANKINGS - {user.mention}:\n`removed {artist} - {album.name} from their rankings`")
 
     # in an added homework album, the changes parameter is the album_id, then the user who initiated them
     @check_decorator
     async def event_add_homework(self, user, album_id, user_affected, users):
         if user not in users:
             await self.channel.send(f"{user.mention} has just used ranking bot for the first time lfggggggg")
-        row = get_album(album_id=album_id)
-        artist = ", ".join(row[0])
+        album = get_album(album_id=album_id)
+        artist = ", ".join([artist.name for artist in album.artists])
         if user.id == user_affected.id:
-            await self.channel.send(f"HOMEWORK - {user.mention}:\n`added {artist} - {row[1]} to their homework list`")
+            await self.channel.send(f"HOMEWORK - {user.mention}:\n`added {artist} - {album.name} to their homework list`")
         else:
-            await self.channel.send(f"HOMEWORK - {user.mention}:\n`added {artist} - {row[1]} to {user_affected.mention}'s homework list`")
+            await self.channel.send(f"HOMEWORK - {user.mention}:\n`added {artist} - {album.name} to {user_affected.mention}'s homework list`")
 
     # in a finished homework album, the changes parameter is the album_id
     # we have to use spotify api because it may have been removed from album_master
     @check_decorator
     async def event_finish_homework(self, user, album_id):
-        row = get_album(album_id=album_id)
-        artist = ", ".join(row[0])
-        await self.channel.send(f"HOMEWORK - {user.mention}:\n`listened to {artist} - {row[1]}`")
+        album = get_album(album_id=album_id)
+        artist = ", ".join([artist.name for artist in album.artists])
+        await self.channel.send(f"HOMEWORK - {user.mention}:\n`listened to {artist} - {album.name}`")
 
     @check_decorator
     async def event_add_bulk(self, user):
