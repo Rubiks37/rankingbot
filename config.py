cc5f09539e80317d524041ffa8e155beab50f099
Rubiks37 <jasxy4321@gmail.com>
2024-02-07 14:06:47 -0500

    Updated getting and setting methods so that they make more sense

--- config.py
+++ config.py
@@ -4,36 +4,22 @@ from json import loads, dumps
 class Config:
     def __init__(self):
         with open('config.json') as file:
-            self.data = loads(file.read())
+            self._config = loads(file.read())
 
     def __getattr__(self, name: str):
-        attribute = self.data.get(name.upper())
-        if not attribute:
-            raise AttributeError("error: config does not have a value called " + name)
-        return attribute
-
-    def __setattr__(self, name, value):
-        if name == 'data':
-            self.__dict__['data'] = value
-            return
         try:
-            self.data[name.upper()] = value
-            with open('config.json', 'w') as file:
-                file.write(dumps(self.data, indent=3))
+            return self._config[name.upper()]
         except KeyError:
-            raise KeyError(name + ' is not a valid config setting')
+            raise AttributeError(name + ' is not a valid config setting')
 
-
-# needs config.py to exist with already existing settings valid
-def import_config_py_to_json():
-    final_dict = {}
-
-    with open("old_config.py") as file:
-        for line in file.readlines():
-            if '=' in line:
-                var_name, var_value = line.split('=', 1)
-                final_dict[var_name.strip()] = var_value.strip().strip('"')
-
-    with open('config.json', 'w') as file:
-        json_object = dumps(final_dict, indent=3)
-        file.write(json_object)
+    def set_attribute(self, name: str, value):
+        try:
+            self._config[name] = value
+            data = dumps(self._config, indent=3)
+            # i don't actually know if this will ever be called
+            if not data:
+                raise AttributeError('config settings are empty, cannot set attribute')
+            with open('config.json', 'w') as file:
+                file.write(data)
+        except KeyError:
+            raise AttributeError(name + ' is not a valid config setting')
